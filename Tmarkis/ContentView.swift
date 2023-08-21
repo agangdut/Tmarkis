30b55e936646a78cbbf87e1b64e09123a7aabdac
thejokester1 <agangdut@gmail.com>
2023-08-20 23:10:28 -0700

    init2

--- Tmarkis/ContentView.swift
+++ Tmarkis/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("test2")
+            Text("test22")
         }
         .padding()
     }

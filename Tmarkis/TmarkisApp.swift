10663c19ef37d3dc3b3096e28fce59670567a405
thejokester1 <agangdut@gmail.com>
2023-08-20 22:29:30 -0700

    Initial Commit

---
+++ Tmarkis/TmarkisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TmarkisApp.swift
+//  Tmarkis
+//
+//  Created by Agang Dut on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TmarkisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
